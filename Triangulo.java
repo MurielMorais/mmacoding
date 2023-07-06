b7ce2ae931676a26efa25410e33bcece89557e44
Muriel Morais Assis <98630758+MurielMorais@users.noreply.github.com>
2023-07-06 16:25:53 -0300

    Add files via upload

---
+++ Triangulo.java
@@ -0,0 +1,35 @@
+package triangulo;
+
+public class Triangulo {
+
+			int altura;
+			int base;
+			
+			public Triangulo() {this(0,0);}
+			
+		
+			public Triangulo(int base, int altura) {
+				// TODO Auto-generated constructor stub
+			}
+
+
+			double getarea() {
+				return ((base*altura)/2.0);
+			}
+			public int getaltura() {
+				return altura;
+			}
+		public void setAltura() {
+				this.altura=altura;
+			}
+
+			public int getBase() {
+				return base;
+			}
+			public void setBase() {
+				this.altura=altura;
+			}
+			public String tostring() {
+				return(["A altura é:"+Triangulo.)
+			}
+}
